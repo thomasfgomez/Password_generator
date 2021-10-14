ec9ef814015dd3161c9e872b11e93c68d05a7529
thomasfgomez <thomasgomez.ar@gmail.com>
2021-10-14 13:31:10 -0300

    v.01 counter

---
+++ contador.py
@@ -0,0 +1,17 @@
+def run():
+    limite = 1000
+
+    contador = 0 
+    potencia_2 = 2 ** contador 
+    while potencia_2 < limite:
+        print('2 ** ' + str(contador) + '= ' + str(potencia_2))
+        contador = contador + 1
+        potencia_2 = 2 ** contador
+
+
+
+
+if __name__ == '__main__':
+    run()    
+
+
